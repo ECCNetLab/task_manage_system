732ccb2be6ce660b022056e4ddd6088da88699ea
coffeejunjun <coffeesugarsi5@gmail.com>
2020-02-19 17:09:46 +0900

    [fix] fix indent and add search form

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,11 +3,10 @@
 
 @section('content')
 <div class="container mt-4">
-    <h1 class="h5 mb-4">Hello {{ Auth::user()->name }}さん</h1>
-    <div class="mt-5">
-        <a class="btn btn-primary" href="{{ route('tasks.create') }}">新規問題作成</a>
-
-    </div>
+  <h1 class="h5 mb-4">Hello {{ Auth::user()->name }}さん</h1>
+  <div class="mt-5">
+    <a class="btn btn-primary" href="{{ route('tasks.create') }}">新規問題作成</a>
+  </div>
 </div>
 
 
