6d144c265b69d95707c7b9af4873d297c524da3a
coffeejunjun <coffeesugarsi5@gmail.com>
2020-02-21 09:17:17 +0900

    [fix] fix indent

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,11 +3,11 @@
 
 @section('content')
 <div class="container mt-4">
-    <h1 class="h5 mb-4">Hello {{ Auth::user()->name }}さん</h1>
-    <div class="mt-5">
-        <a class="btn btn-primary" href="{{ route('tasks.create') }}">新規問題作成</a>
+	<h1 class="h5 mb-4">Hello {{ Auth::user()->name }}さん</h1>
+	<div class="mt-5">
+		<a class="btn btn-primary" href="{{ route('tasks.create') }}">新規問題作成</a>
 
-    </div>
+	</div>
 </div>
 
 
