053848ce4fabd250aa12239644ef99057e8f38d6
coffeejunjun <coffeesugarsi5@gmail.com>
2020-04-06 15:18:33 +0900

    [edit] 自分が書いたものだけ編集、削除できるようにした

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -9,24 +9,27 @@
 				<h3 style="display:inline">{{ $task->title }}</h3>
 			</div>
 			<div class="float-right">
-				<a class="btn btn-info" href="#">
-					<i class="fas fa-pencil-alt"></i>Edit
-				</a>
-				<a class="btn btn-danger" href="#">
-					<i class="fas fa-trash"></i>Delete
-				</a>
+				@auth
+					@if(Auth::user()->id === $user->id)
+						<a class="btn btn-info" href="{{ route('tasks.edit',$task->id) }}">
+							<i class="fas fa-pencil-alt"></i>Edit
+						</a>
+						<a class="btn btn-danger" href="#">
+							<i class="fas fa-trash"></i>Delete
+						</a>
+					@endif
+				@endauth
 			</div>
 		</div>
 		<div class="card-body">
-			<h5>{!! nl2br($task->body) !!}</h5>
+			<h5>{!! nl2br(e($task->body)) !!}</h5>
 			Tag @foreach($task->tags as $tag)
-			<a class="btn btn-primary btn-sm bg-white">{{ $tag->name }}</a>
+				<a class="btn btn-primary btn-sm bg-white">{{ $tag->name }}</a>
 			@endforeach
 		</div>
-		
 		<div class="card-footer">
 			<div class="float-right d-none d-sm-inline-block">
-			Create User {{ $user->name }}
+				Create User {{ $user->name }}
 			</div>
 		</div>
 	</div>
