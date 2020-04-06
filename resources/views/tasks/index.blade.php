9435beed198ae4f2200d9ea6517a91b4df17c921
coffeejunjun <coffeesugarsi5@gmail.com>
2020-04-06 11:58:45 +0900

    [add] ページネーションを追加

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,30 +5,38 @@
 <div class="container mt-4">
 	<div class="mb-4">
 		<div class="h3">Hello {{ Auth::user()->name }}さん
-		<a class="btn btn-primary float-right" href="{{ route('tasks.create') }}">新規問題作成</a>
+			<a class="btn btn-primary float-right" href="{{ route('tasks.create') }}">新規問題作成</a>
+		</div>
 	</div>
-</div>
-	
-	<div class="mt-5">	
-		@if($tasks !== null)
-		@foreach($tasks as $task)
-		<div class="card card-default">
-		<div class="card-header">
-			<h3 class="card-title"><a href="{{ route('tasks.show',$task->id) }}">{{$task->title}}</a></h3>
-		</div>
-		<div class="card-body">
-			<h5>{{$task->body}}</h5>
-			Tag @foreach($task->tags as $tag)
-			<a class="btn btn-primary btn-sm bg-white">{{ $tag->name }}</a>
-			@endforeach		
-		</div>
-		<div class="card-footer">
-			<div class="float-right d-none d-sm-inline-block">
-			Create User {{ $task->create_user }}
+	<div class="mt-5">
+		@if($tasks ?? '')
+			@foreach($tasks ?? '' as $task)
+			<div class="card card-default">
+				<div class="card-header">
+					<h3 class="card-title"><a href="{{ route('tasks.show',$task->id) }}">{{$task->title}}</a></h3>
+				</div>
+				<div class="card-body">
+					<h5>{!! nl2br($task->body) !!}</h5>
+						Tag @foreach($task->tags as $tag)
+							<a class="btn btn-primary btn-sm bg-white">{{ $tag->name }}</a>
+						@endforeach		
+				</div>
+				<div class="card-footer">
+					<div class="float-right d-none d-sm-inline-block">
+						Create User {{ $task->create_user }}
+					</div>
+				</div>
 			</div>
-		</div>
-		@endforeach
+			@endforeach
 		@endif
 	</div>
 </div>
+<footer class="footer">
+	<nav aria-label="Contacts Page Navigation">
+		<ul class="pagination justify-content-center m-0">
+			{{ $tasks->links() }}
+		</ul>
+	</nav>
+</footer>
+
 @endsection
